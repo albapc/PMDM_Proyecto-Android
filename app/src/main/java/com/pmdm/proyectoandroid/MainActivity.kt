7234332f0a0c48d24097bdcdac97de39aa32637e
Alba <alba.perez.ce@gmail.com>
2019-12-08 17:39:51 +0100

    acabada reto 4 giroscopio y sensor proximidad

--- app/src/main/java/com/pmdm/proyectoandroid/MainActivity.kt
+++ app/src/main/java/com/pmdm/proyectoandroid/MainActivity.kt
@@ -13,13 +13,14 @@ import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
 import android.graphics.Bitmap
+import android.os.Handler
 import org.jetbrains.anko.longToast
 
 const val QUESTION_REQUEST = 1
 const val CAMERA_REQUEST = 2
 const val REQUEST_IMAGE_CAPTURE = 3
 const val OPERATION_REQUEST = 4
-const val LOCATION_REQUEST = 5
+const val SENSOR_REQUEST = 5
 
 class MainActivity : AppCompatActivity() {
 
@@ -51,6 +52,43 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        if(!reto1.isEnabled && !reto2.isEnabled && !reto3.isEnabled && !reto4.isEnabled) {
+            val alertDialog = AlertDialog.Builder(this).create()
+            alertDialog.setTitle("Enhorabuena!")
+            alertDialog.setMessage("Ganaste el juego. Quieres jugar otra vez?")
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Si") { dialog, _ ->
+                volverAJugar()
+            }
+
+            alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No") { dialog, _ ->
+                dialog.run {
+                    finish()
+                }
+            }
+            alertDialog.show()
+        }
+    }
+
+
+    fun volverAJugar() {
+        reto1.isEnabled = true
+        reto2.isEnabled = true
+        reto3.isEnabled = true
+        reto4.isEnabled = true
+
+        reto1.setImageResource(R.drawable.yellowstar)
+        reto2.setImageResource(R.drawable.yellowstar)
+        reto3.setImageResource(R.drawable.yellowstar)
+        reto4.setImageResource(R.drawable.yellowstar)
+
+        reto1.setBackgroundColor(Color.parseColor("#ff8ba7"))
+        reto2.setBackgroundColor(Color.parseColor("#ff8ba7"))
+        reto3.setBackgroundColor(Color.parseColor("#ff8ba7"))
+        reto4.setBackgroundColor(Color.parseColor("#ff8ba7"))
+    }
 
     fun mostrarReto1() {
         val questionIntent = Intent(this, Reto1Activity::class.java)
@@ -86,11 +124,23 @@ class MainActivity : AppCompatActivity() {
 
     fun mostrarReto4() {
         if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST)
+            requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), SENSOR_REQUEST)
         }
         val miIntent = Intent(this, Reto4Activity::class.java)
 
-        startActivityForResult(miIntent, LOCATION_REQUEST)
+        val alertDialog = AlertDialog.Builder(this).create()
+
+        alertDialog.setTitle("Nota")
+        alertDialog.setMessage("En este reto tienes que activar el sensor de proximidad o el giroscopio.")
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK") {
+                dialog, _ -> dialog.dismiss()
+        }
+        alertDialog.show()
+        alertDialog.setOnDismissListener {
+            startActivityForResult(miIntent, SENSOR_REQUEST)
+        }
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -135,6 +185,18 @@ class MainActivity : AppCompatActivity() {
                     Activity.RESULT_CANCELED ->
                         Log.d("miApp", "Cancelado")
                 }
+            SENSOR_REQUEST ->
+                when(resultCode) {
+                    Activity.RESULT_OK ->
+                        if(data != null) {
+                            toast("Reto 4 superado!")
+                            reto4.isEnabled = false
+                            reto4.setImageResource(R.drawable.colorstar)
+                            reto4.setBackgroundColor(Color.parseColor("#c3f0ca"))
+                        }
+                    Activity.RESULT_CANCELED ->
+                        Log.d("miApp", "Cancelado")
+                }
         }
     }
 
@@ -154,13 +216,6 @@ class MainActivity : AppCompatActivity() {
                         toast("camera permission denied. The app needs permissions to run.")
                 }
             }
-            LOCATION_REQUEST ->
-                when(grantResults[0]) {
-                    PackageManager.PERMISSION_GRANTED ->
-                        toast("location permission granted")
-                    PackageManager.PERMISSION_DENIED ->
-                        toast("location permission denied. The app needs permissions to run.")
-                }
         }
     }
 }
--- app/src/main/java/com/pmdm/proyectoandroid/Reto4Activity.kt
+++ app/src/main/java/com/pmdm/proyectoandroid/Reto4Activity.kt
@@ -1,12 +1,133 @@
 package com.pmdm.proyectoandroid
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
 
 class Reto4Activity : AppCompatActivity() {
+
+    private var sensorManager: SensorManager? = null
+    private var proximitySensor: Sensor? = null
+    private var proximitySensorListener: SensorEventListener? = null
+    private var gyroscopeSensorListener: SensorEventListener? = null
+    private var gyroscopeSensor: Sensor? = null
+    private var data = Intent()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reto4)
+
+        probarProximidad()
+
+        probarGiroscopio()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        // Registra el intervalo de cambio en milisegundos
+        sensorManager?.registerListener(
+            proximitySensorListener,
+            proximitySensor, 2 * 1000 * 1000
+
+        )
+
+        //Registra el listener
+        sensorManager?.registerListener(
+            gyroscopeSensorListener,
+            gyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager?.unregisterListener(proximitySensorListener)
+    }
+
+    private fun probarProximidad() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        proximitySensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_PROXIMITY) as Sensor
+
+        if (proximitySensor == null) {
+            Log.d("miApp", "Proximity sensor not available.")
+            finish() // Cerrar app
+        }
+
+        // Crea el listener
+        proximitySensorListener = object : SensorEventListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            }
+
+            override fun onSensorChanged(sensorEvent: SensorEvent) {
+                if (sensorEvent.values[0] < proximitySensor!!.maximumRange) {
+                    // Detecto algo cerca
+                    window.decorView.setBackgroundColor(Color.RED)
+
+                    data.putExtra("conseguido", true)
+                    setResult(Activity.RESULT_OK, data)
+                    Handler().postDelayed(
+                        {
+                            finish()
+                        }, 1500
+                    )
+                } else {
+                    // No hay nada cerca
+                    window.decorView.setBackgroundColor(Color.GREEN)
+                }
+            }
+        }
+    }
+
+    private fun probarGiroscopio() {
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        gyroscopeSensor = sensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE) as Sensor
+
+        if (gyroscopeSensor == null) {
+            Log.d("miApp", "Proximity sensor not available.")
+            finish() // Cerrar app
+
+        }
+
+        // Create a listener
+        gyroscopeSensorListener = object : SensorEventListener {
+            override fun onSensorChanged(sensorEvent: SensorEvent) {
+                if (sensorEvent.values[2] > 0.5f) { // anticlockwise
+                    window.decorView.setBackgroundColor(Color.BLUE)
+                    data.putExtra("conseguido", true)
+                    setResult(Activity.RESULT_OK, data)
+                    Handler().postDelayed(
+                        {
+                            finish()
+                        }, 1500
+                    )
+                } else if (sensorEvent.values[2] < -0.5f) { // clockwise
+                    window.decorView.setBackgroundColor(Color.YELLOW)
+                    data.putExtra("conseguido", true)
+                    setResult(Activity.RESULT_OK, data)
+                    Handler().postDelayed(
+                        {
+                            finish()
+                        }, 1500
+                    )
+                }
+            }
+
+            override fun onAccuracyChanged(sensor: Sensor, i: Int) {}
+        }
     }
 }
+
